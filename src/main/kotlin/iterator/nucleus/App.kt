dcca552c58a4ea6e4f852133bd702bf99099d238
Christopher Townson <christopher.townson@atombank.co.uk>
2025-05-25 19:30:14 +0200

    chore: remove redundant extension of WebMvcConfigurer

--- src/main/kotlin/iterator/nucleus/App.kt
+++ src/main/kotlin/iterator/nucleus/App.kt
@@ -2,15 +2,10 @@ package iterator.nucleus
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@SpringBootApplication
-class App : WebMvcConfigurer {
-  companion object {
-    @JvmStatic
-    @Suppress("SpreadOperator")
-    fun main(args: Array<String>) {
-      runApplication<App>(*args)
-    }
-  }
+@SpringBootApplication class App
+
+@Suppress("SpreadOperator")
+fun main(args: Array<String>) {
+  runApplication<App>(*args)
 }
